26b2cb150d7c57f83b6eb6ed73aa7bd319546ea1
Vitor Andrade Monteiro <monteirovitor10@hotmail.com>
2021-03-16 06:05:40 -0300

    comitado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deucerto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
